2163199c1be3168b7580bd71f51c1e863ff39a63
JakubForys4 <jakubforys09@gmail.com>
2025-04-30 14:52:41 +0000

    5_3

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -5,12 +5,13 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
 public class Service {
 
   public void addStudent(Student student) throws IOException {
     var f = new FileWriter("db.txt", true);
     var b = new BufferedWriter(f);
-    b.append(student.ToString());
+    b.append(student.toString());
     b.newLine();
     b.close();
   }
@@ -22,7 +23,7 @@ public class Service {
     String line = "";
     while (true) {
       line = reader.readLine();
-      if(line == null)
+      if (line == null)
         break;
       ret.add(Student.Parse(line));
     }
@@ -45,29 +46,30 @@ public class Service {
     System.out.println("Nie znaleziono studenta o imieniu: " + name);
     return null;
   }
+
   public boolean removeStudent(String name, String lastName) throws IOException {
     Collection<Student> students = getStudents();
     boolean studentRemoved = false;
-     ArrayList<Student> updatedStudents = new ArrayList<>();
-        for (Student student : students) {
-          if (!(student.GetName().equalsIgnoreCase(name) && student.GetLastName().equalsIgnoreCase(lastName))) {
-            updatedStudents.add(student);
-          } else {
-            studentRemoved = true;
-          }
-        }
-
-        if (studentRemoved) {
-          
-          try (BufferedWriter writer = new BufferedWriter(new FileWriter("db.txt"))) {
-            for (Student student : updatedStudents) {
-              writer.append(student.toString());
-              writer.newLine();
-            }
-          } catch (IOException e) {
-            throw new IOException("Błąd przy zapisie do pliku.", e);
-          }
-        }
-        return studentRemoved;
+    ArrayList<Student> updatedStudents = new ArrayList<>();
+    for (Student student : students) {
+      if (!(student.GetName().equalsIgnoreCase(name) && student.GetLastName().equalsIgnoreCase(lastName))) {
+        updatedStudents.add(student);
+      } else {
+        studentRemoved = true;
       }
     }
+
+    if (studentRemoved) {
+
+      try (BufferedWriter writer = new BufferedWriter(new FileWriter("db.txt"))) {
+        for (Student student : updatedStudents) {
+          writer.append(student.toString());
+          writer.newLine();
+        }
+      } catch (IOException e) {
+        throw new IOException("Błąd przy zapisie do pliku.", e);
+      }
+    }
+    return studentRemoved;
+  }
+}
